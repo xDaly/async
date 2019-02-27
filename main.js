9476fd55dcffd9f32a3b60b28cd35cc8f98bc0ff
Med Ali Ezzeddine <47082236+xDaly@users.noreply.github.com>
2019-02-27 17:22:56 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-
-
-const plotPerson = ({name, roles, phone, email}) => (
-  `<div class="col">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">${name}</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">${roles.map(role => 
-                    `<span style="text-transform: capitalize;">${role} </span>`
-                    )}</h6>
-                  <a href="tel:${phone}" class="card-link">Phone</a>
-                  <a href="mailto:${email}" class="card-link">Email</a>
-                </div>
-              </div>
-            </div>`
+const plotPerson = ({ name, roles, phone, email }) => (
+        `<div class="col">
+<div class="card">
+<div class="card-body">
+<h5 class="card-title">${name}</h5>
+<h6 class="card-subtitle mb-2 text-muted">${roles.map(role => 
+`<span style="text-transform: capitalize;">${role} </span>`
+)}</h6>
+<a href="tel:${phone}" class="card-link">Phone</a>
+<a href="mailto:${email}" class="card-link">Email</a>
+</div>
+</div>
+</div>`
 );
 
 const fetchPersonsFromAPI = async () => {
-    const response = await fetch('https://my-json-server.typicode.com/horizon-code-academy/fake-data-api/users');
-    const data = await response.json();
-    document.querySelector('.row').innerHTML = data.map(person => plotPerson(person)).join('');
+const response = await fetch('https://my-json-server.typicode.com/horizon-code-academy/fake-data-api/users');
+const data = await response.json();
+
+if(!localStorage.getItem("person")){
+localStorage.setItem("person", data.map(person => plotPerson(person)).join(''));	
+}
+document.querySelector('.row').innerHTML = localStorage.getItem("person");
 }
 
 fetchPersonsFromAPI();
@@ -31,23 +33,17 @@ fetchPersonsFromAPI();
 
 
 document.querySelector('#submit').addEventListener("click",() =>{
-  const name = document.querySelector('#name').value ;
-  const role = document.querySelector('#role').value  ;
-  const phone = document.querySelector('#phone').value  ;
-  const email = document.querySelector('#email').value  ;
+const name = document.querySelector('#name').value ;
+const roles = [document.querySelector('#role').value] ;
+const phone = document.querySelector('#phone').value ;
+const email = document.querySelector('#email').value ;
 
-  const persons = {name, role, phone, email}  ;
-
-
-  const fetchPersonsFromLocalStorage = () => {
-    const persons = JSON.parse(localStorage.getItem("person"));
-    document.querySelector('.row').innerHTML = persons.map(person => plotPerson(persons)).join('');
-  };
+const persons = {name, roles, phone, email} ;
 
 
+const prs = localStorage.getItem("person") + "" + plotPerson(persons);
+localStorage.setItem("person", prs)
 
-fetchPersonsFromLocalStorage();
-});  
+document.querySelector('.row').innerHTML = localStorage.getItem("person");
 
-
-
+});
